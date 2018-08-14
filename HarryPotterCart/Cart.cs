7966b4f13a79023f74bf28eb86ecfc1bf1c68563
LeoSheu <leo850611@hotmail.com>
2018-08-14 10:41:24 +0800

    buy ep1234 pass

--- HarryPotterCart/Cart.cs
+++ HarryPotterCart/Cart.cs
@@ -10,7 +10,7 @@ namespace HarryPotterCart
 	{
 		private Dictionary<int, double> _discountLookUp = new Dictionary<int, double>()
 		{
-			{ 1, 1 }, { 2, 0.95 }, { 3, 0.9 }
+			{ 1, 1 }, { 2, 0.95 }, { 3, 0.9 },{ 4, 0.8 }
 		};
 
 		public double Checkout(Dictionary<string, int> books)
