e84711423b8803ade3395e639b34ea1bf31cd15c
LeoSheu <leo850611@hotmail.com>
2018-08-14 11:49:41 +0800

    buy EP112 290 pass

--- HarryPotterCart/Cart.cs
+++ HarryPotterCart/Cart.cs
@@ -8,15 +8,23 @@ namespace HarryPotterCart
 {
 	public class Cart
 	{
+		public double Checkout(Dictionary<string, int> books)
+		{
+			var total = 0d;
+			var maxBook = books.Max(p => p.Value);
+			for (int i = maxBook; i > 0; i--)
+			{
+				var count = books.Count(p => p.Value >= i);
+				var money = 100 * count;
+				total += money * _discountLookUp[count];
+			}
+			return total;
+		}
+
 		private Dictionary<int, double> _discountLookUp = new Dictionary<int, double>()
 		{
 			{ 1, 1 }, { 2, 0.95 }, { 3, 0.9 }, { 4, 0.8 }, { 5, 0.75 }
 		};
 
-		public double Checkout(Dictionary<string, int> books)
-		{
-			var money = 100 * books.Count;
-			return money * _discountLookUp[books.Count];
-		}
 	}
 }
